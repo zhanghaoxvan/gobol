3f708c681b3e0865ca94311f9a02f36d6b6a92fe
zhanghaoxvan <3592114583@qq.com>
2026-01-28 22:22:16 +0800

    Add TokenType

--- Lexer/Token.hpp
+++ Lexer/Token.hpp
@@ -7,6 +7,26 @@
 
 #include <string>
 
+enum class TokenType {
+    IDENTIFIER,       // like variable, function, class...
+    NUMBER,           // like 1, 2, 3, 4, 114514...
+    STRING,           // like "Hello world!"...
+    OP_PLUS,          // +
+    OP_MINUS,         // -
+    OP_MULT,          // *
+    OP_DIV,           // /
+    OP_LEFT_BRACKET,  // (
+    OP_RIGHT_BRACKET, // )
+    OP_LEFT_BRACE,    // {
+    OP_RIGHT_BRACE,   // }
+    OP_LEFT_SQUARE,   // [
+    OP_RIGHT_SQUARE,  // ]
+    OP_ASSIGN,        // =
+    OP_FORMAT,        // @ in @STRING (STRING is "... {IDENFIER} ...")
+    END_OF_LINE,      // \n
+    END_OF_FILE,      // \0
+};
+
 namespace gobol {
     class Token {};
 } // namespace gobol
